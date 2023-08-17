2f04938844950bd72f3e48c7f336cc65e0d7d64c
kyle08080000 <s106441109@gmail.com>
2023-08-17 17:24:15 +0800

    add purchaseorder.html

--- js/all.js
+++ js/all.js
@@ -21,3 +21,24 @@
         });
     });
 
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+// 表單驗證
+(function () {
+    'use strict'
+
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    var forms = document.querySelectorAll('.needs-validation')
+
+    // Loop over them and prevent submission
+    Array.prototype.slice.call(forms)
+        .forEach(function (form) {
+        form.addEventListener('submit', function (event) {
+            if (!form.checkValidity()) {
+            event.preventDefault()
+            event.stopPropagation()
+            }
+
+            form.classList.add('was-validated')
+        }, false)
+        })
+})()
